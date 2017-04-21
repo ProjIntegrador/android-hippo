677cda999772207f133ed1cb20f92077f18c5d89
Renato <renatoea@gmail.com>
2017-04-21 20:03:32 -0300

    First Commit

---
+++ app/src/main/java/br/com/enforce/hippov1/MainActivity.java
@@ -0,0 +1,51 @@
+package br.com.enforce.hippov1;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox termos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final SharedPreferences prefs = getSharedPreferences("MeusDados", MODE_PRIVATE);
+
+        String configuracao = prefs.getString("termos", null);
+        termos = (CheckBox) findViewById(R.id.termos);
+        if(configuracao==null){
+
+
+            termos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    if(isChecked) {
+
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("termos", "ok");
+                        editor.apply();
+                        //vai para outra
+                        Intent intent = new Intent(MainActivity.this, Categorias.class);
+                        startActivity(intent);
+
+                    }else{
+
+                    }
+
+
+                }
+            });
+
+
+        }else{
+            // vai para outra
+            Intent intent = new Intent(this, Categorias.class);
+            startActivity(intent);
+        }
+    }}
