18a4a27eeeee3da0c9e7f88a5a6a56854479b441
Renato <renatoea@gmail.com>
2017-04-25 08:36:27 -0300

    Criação da tela -  Descrição do Produto

---
+++ app/src/main/java/br/com/enforce/hippov1/DescricaoProduto.java
@@ -0,0 +1,13 @@
+package br.com.enforce.hippov1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DescricaoProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descricao_produto);
+    }
+}
