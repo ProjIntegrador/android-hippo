8a33f4808eb40d169efd175da3ebbfabb4aab55b
henrique.prigio1 <hp.rigio@gmail.com>
2017-05-03 22:01:02 -0300

    criado tela de cadastro

---
+++ app/src/main/java/br/com/enforce/hippov1/rest/Cadastro.java
@@ -0,0 +1,15 @@
+package br.com.enforce.hippov1.rest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.enforce.hippov1.R;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
