4d093586aed19c84181c6a2fc3dcb999874b96a6
Cinthia Inhaia <cinthia.inhaia@hotmail.com>
2017-05-10 22:01:42 -0300

    criado a Classe UsuarioAuth comunicação com webservice comunicação do xml com o java da activity cadastro ajuste no login

--- app/src/main/java/br/com/enforce/hippov1/rest/Cadastro.java
+++ app/src/main/java/br/com/enforce/hippov1/rest/Cadastro.java
@@ -2,14 +2,35 @@ package br.com.enforce.hippov1.rest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import br.com.enforce.hippov1.R;
 
 public class Cadastro extends AppCompatActivity {
 
+    private EditText nomeCompletoCliente;
+    private EditText emailCliente;
+    private EditText senhaCliente;
+    private EditText celularCliente;
+    private EditText dtNascCliente;
+    private CheckBox cbx_promosao_por_email;
+    private Button btn_cadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nomeCompletoCliente = (EditText) findViewById(R.id.nomeCompletoCliente);
+        emailCliente = (EditText) findViewById(R.id.emailCliente);
+        senhaCliente = (EditText) findViewById(R.id.senhaUsuario);
+        celularCliente = (EditText) findViewById(R.id.celularCliente);
+        dtNascCliente = (EditText)  findViewById(R.id.dtNascCliente);
+        cbx_promosao_por_email  = (CheckBox) findViewById(R.id.cbx_promosao_por_email);
+        btn_cadastrar = (Button) findViewById(R.id.btn_cadastrar);
+
     }
+
 }
---
+++ app/src/main/java/br/com/enforce/hippov1/rest/UsuarioAuth.java
@@ -0,0 +1,12 @@
+package br.com.enforce.hippov1.rest;
+
+
+public class UsuarioAuth {
+
+    private Long id;
+    private String loginUsuario;
+    private String senhaUsuario;
+    private String nomeUsuario;
+    private String tipoPerfil;
+    private boolean usuarioAtivo;
+}
