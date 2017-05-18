c01c9ac3c906b8320f3f37e9f547bc8e3aa50c17
Cinthia Inhaia <cinthia.inhaia@hotmail.com>
2017-05-17 22:12:11 -0300

    Merge branch 'master' of C:\Users\casasbahia\p.i4\android-hippo with conflicts.

---
+++ app/src/main/java/br/com/enforce/hippov1/Carrinho.java
@@ -0,0 +1,22 @@
+package br.com.enforce.hippov1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Carrinho extends AppCompatActivity {
+
+    private TextView nomeProduto;
+    private TextView nomeProduto1;
+    private TextView nomeProduto2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_carrinho);
+
+
+    }
+}
