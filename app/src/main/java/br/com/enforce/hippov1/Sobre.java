0682a5afbf17c338509489bb06e6d6a4d6249e3d
henrique.prigio1 <hp.rigio@gmail.com>
2017-05-18 22:26:53 -0300

    sobre incompleto

---
+++ app/src/main/java/br/com/enforce/hippov1/Sobre.java
@@ -0,0 +1,13 @@
+package br.com.enforce.hippov1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
