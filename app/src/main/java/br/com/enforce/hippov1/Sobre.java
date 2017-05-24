1eb08dcce3f8211dde4e019e676f39420321070c
henriqueprieto <hp.rigio@gmail.com>
2017-05-24 00:10:39 -0300

    sobre quase la

--- app/src/main/java/br/com/enforce/hippov1/Sobre.java
+++ app/src/main/java/br/com/enforce/hippov1/Sobre.java
@@ -1,13 +1,55 @@
 package br.com.enforce.hippov1;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class Sobre extends AppCompatActivity {
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+public class Sobre extends Activity {
+    private ViewGroup cadsimages1;
+    private ViewGroup cadsimages2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+        cadsimages1 = (ViewGroup) findViewById(R.id.container1);
+        cadsimages2 = (ViewGroup) findViewById(R.id.conteiner2);
+
+        addItem1("https://s-media-cache-ak0.pinimg.com/originals/80/6d/3b/806d3bffaaa73470dd38b5eaccd47f23.png");
+
+        addItem1("https://s-media-cache-ak0.pinimg.com/originals/80/6d/3b/806d3bffaaa73470dd38b5eaccd47f23.png");
+
+        addItem2("https://s-media-cache-ak0.pinimg.com/originals/80/6d/3b/806d3bffaaa73470dd38b5eaccd47f23.png");
+
+        addItem2("https://s-media-cache-ak0.pinimg.com/originals/80/6d/3b/806d3bffaaa73470dd38b5eaccd47f23.png");
+
     }
-}
+
+    private void addItem1(String urls) {
+
+
+        CardView cardView =(CardView) LayoutInflater.from(this).inflate(R.layout.activity_itens_cards_sobre,cadsimages1,false);
+        ImageView imagens = (ImageView) cardView.findViewById(R.id.imageView);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+        imageLoader.displayImage(urls, imagens);
+        cadsimages1.addView(cardView);
+    }
+
+    private void addItem2(String urls) {
+
+
+        CardView cardView =(CardView) LayoutInflater.from(this).inflate(R.layout.activity_itens_cards_sobre,cadsimages2,false);
+        ImageView imagens = (ImageView) cardView.findViewById(R.id.imageView);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+        imageLoader.displayImage(urls, imagens);
+        cadsimages2.addView(cardView);
+    }
+    }
