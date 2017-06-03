94bb391cfc2cbd1d6a8fe3de5b11f268525f0f43
renatoea <renatoea@gmail.com>
2017-06-03 17:42:07 -0300

    comunicacao inicial do cadastro de cliente

--- app/src/main/java/br/com/enforce/hippov1/Cadastro.java
+++ app/src/main/java/br/com/enforce/hippov1/Cadastro.java
@@ -1,12 +1,21 @@
 package br.com.enforce.hippov1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-import br.com.enforce.hippov1.R;
+import br.com.enforce.hippov1.rest.ClienteLoginRest;
+import br.com.enforce.hippov1.rest.HippoServices;
+import br.com.enforce.hippov1.rest.RetrofitInitializer;
+import br.com.enforce.hippov1.tempdata.SingletonHippo;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -14,7 +23,7 @@ public class Cadastro extends AppCompatActivity {
     private EditText emailCliente;
     private EditText senhaCliente;
     private EditText celularCliente;
-    private EditText dtNascCliente;
+    private EditText cpfCliente;
     private CheckBox cbx_promosao_por_email;
     private Button btn_cadastrar;
 
@@ -27,10 +36,52 @@ public class Cadastro extends AppCompatActivity {
         emailCliente = (EditText) findViewById(R.id.emailCliente);
         senhaCliente = (EditText) findViewById(R.id.senhaCliente);
         celularCliente = (EditText) findViewById(R.id.celularCliente);
-        dtNascCliente = (EditText)  findViewById(R.id.dtNascCliente);
-        cbx_promosao_por_email  = (CheckBox) findViewById(R.id.cbx_promosao_por_email);
+        cpfCliente = (EditText) findViewById(R.id.cpfCliente);
+        cbx_promosao_por_email = (CheckBox) findViewById(R.id.cbx_promosao_por_email);
         btn_cadastrar = (Button) findViewById(R.id.btn_cadastrar);
 
-    }
 
+        btn_cadastrar.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                ClienteLoginRest clienteLoginRest = new ClienteLoginRest();
+                clienteLoginRest.setNomeCompletoCliente(nomeCompletoCliente.getText().toString());
+                clienteLoginRest.setCpfcliente(cpfCliente.getText().toString());
+                clienteLoginRest.setEmailCliente(emailCliente.getText().toString());
+                clienteLoginRest.setSenhaCliente(senhaCliente.getText().toString());
+                clienteLoginRest.setCelularCliente(celularCliente.getText().toString());
+
+                HippoServices service = new RetrofitInitializer(getApplicationContext()).getHippoServices();
+                Call<ClienteLoginRest> retorno = service.criarUsuario(clienteLoginRest);
+                retorno.enqueue(new Callback<ClienteLoginRest>() {
+                    @Override
+                    public void onResponse(Call<ClienteLoginRest> call, Response<ClienteLoginRest> response) {
+                        if (response.isSuccessful()) {
+                            Log.i("retorno", "raw : " + response.raw().body());
+
+                            ClienteLoginRest cliente = response.body();
+                            SingletonHippo.Instance().setCliente(cliente);
+
+                            Intent intent = new Intent(Cadastro.this, Pagamento.class);
+                            startActivity(intent);
+
+                            Log.i("retorno", "id : " + cliente.getIdCliente());
+                        } else {
+                            Log.i("return_error", response.body().toString());
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ClienteLoginRest> call, Throwable t) {
+                        Log.e("falha", t.getMessage());
+                    }
+
+                });
+
+            }
+
+        });
+
+    }
 }
